8fc40c1b1dc3a880b3921e9be7da8513eecf12e0
vic233333 <93087330+vic233333@users.noreply.github.com>
2026-01-07 00:55:46 +0800

    添加 UMG、Slate 和 SlateCore 模块作为公共依赖项

--- Source/Onet/Onet.Build.cs
+++ Source/Onet/Onet.Build.cs
@@ -7,14 +7,15 @@ public class Onet : ModuleRules
 	public Onet(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
